e6b57396493db9baa4443e243658b6e674a3a43c
AndreyKryzhniov <andreykryzhniov@gmail.com>
2020-01-13 21:56:22 +0300

    Updates

---
+++ precache-manifest.d68f2839f63a61e7179b513f928ee968.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50d4baac4141fe1396ddc5c01ca2457",
+    "url": "/project_social_network_2.0/index.html"
+  },
+  {
+    "revision": "1f4602f5183f0b53ecbe",
+    "url": "/project_social_network_2.0/static/css/3.66c92b46.chunk.css"
+  },
+  {
+    "revision": "130f4149afad77081169",
+    "url": "/project_social_network_2.0/static/css/main.b19ab48c.chunk.css"
+  },
+  {
+    "revision": "02e3744367429c8ec384",
+    "url": "/project_social_network_2.0/static/js/2.64a000cc.chunk.js"
+  },
+  {
+    "revision": "1f4602f5183f0b53ecbe",
+    "url": "/project_social_network_2.0/static/js/3.737b885c.chunk.js"
+  },
+  {
+    "revision": "130f4149afad77081169",
+    "url": "/project_social_network_2.0/static/js/main.06fcf589.chunk.js"
+  },
+  {
+    "revision": "fefe339183f48daf33df",
+    "url": "/project_social_network_2.0/static/js/runtime-main.f98b3536.js"
+  },
+  {
+    "revision": "782adc2b6062ab00461359da5b02b753",
+    "url": "/project_social_network_2.0/static/media/Deafult-Profile-Pitcher.782adc2b.png"
+  },
+  {
+    "revision": "f082f31e48bfa06bfdd225b09d7b3768",
+    "url": "/project_social_network_2.0/static/media/Spinner-1s-200px.f082f31e.svg"
+  },
+  {
+    "revision": "4c0625bed2cfa8d66975dbde28013f8b",
+    "url": "/project_social_network_2.0/static/media/smallAvatar.4c0625be.png"
+  }
+]);
